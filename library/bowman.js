1ccf02f518825ab6c77de07df69755b800155d38
Edward Bai <edwardbai31@gmail.com>
2017-12-08 09:44:15 -0800

    Reorg util file and structure

--- library/bowman.js
+++ library/bowman.js
@@ -1,21 +1,7 @@
-import Game from './game.js';
-import View from './view.js';
-import Arrow from './arrow.js';
+import { distance, angle } from './utils';
 
-document.addEventListener("DOMContentLoaded", ()=> {
-  const canvasEl = document.getElementById("canvas");
-  canvasEl.width = window.innerWidth - 15;
-  canvasEl.height = window.innerHeight - 15;
-  let canvasWidth = canvasEl.width;
-  let canvasHeight = canvasEl.height;
-  let groundPoint = canvasHeight * (3/4);
-  const ctx = canvasEl.getContext("2d");
-  document.body.appendChild(canvasEl);
-  // const arrow = new Arrow();
-  // arrow.draw(ctx);
-  const game = new Game();
-  new View(game, ctx).start();
-});
+var arrows = [];
+var speedDiv = 4;
 
 function Arrow() {
   this.x = shootingCirc.x;
@@ -79,3 +65,19 @@ Arrow.prototype.calcArrowHead = function () {
 
   // console.log(this.arrowTipCoords.x, this.arrowTipCoords.y);
 };
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const canvasEl = document.getElementById("canvas");
+  canvasEl.width = window.innerWidth - 15;
+  canvasEl.height = window.innerHeight - 15;
+  let canvasWidth = canvasEl.width;
+  let canvasHeight = canvasEl.height;
+  let groundPoint = canvasHeight * (3 / 4);
+  const ctx = canvasEl.getContext("2d");
+  document.body.appendChild(canvasEl);
+  // const arrow = new Arrow();
+  // arrow.draw(ctx);
+  const game = new Game();
+  new View(game, ctx).start();
+});
--- library/utils.js
+++ library/utils.js
@@ -1,22 +1,7 @@
-var gravity = 0.5;
-
-var speedMod = 4;
-
-
 export const distance = (p1, p2) => {
-  return Math.sqrt(Math.pow((p2.x - p1.x), 2)
-    + Math.pow((p2.y - p1.y), 2));
+  return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
 };
 
 export const angle = (p1, p2) => {
   return Math.atan2(p2.y - p1.y, p2.x - p1.x);
-};
-
-export const getAimCoords = (mousePos) => {
-  /* NOTE: angleBetween(p1, p2) is 180deg opposite of angleBetween(p2, p1) */
-  var aimAng = Math.PI / 2 - angle(mousePos, shootingCirc);
-  var aimDistance = Math.min(distance(shootingCirc, mousePos), shootingCirc.r);
-  var x = shootingCirc.x + -aimDistance * Math.sin(aimAng);
-  var y = shootingCirc.y + -aimDistance * Math.cos(aimAng);
-  return { x: x, y: y };
 };
